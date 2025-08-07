6836b96aee91e8e3672543cac27dd9e11786aab6
soo59599 <sooh59599@gmail.com>
2025-08-07 14:25:38 +0900

    feat: 회원가입 조건 추가

--- src/main/java/com/spartaschedulerdevelop/dto/ScheduleSaveRequestDto.java
+++ src/main/java/com/spartaschedulerdevelop/dto/ScheduleSaveRequestDto.java
@@ -1,18 +1,22 @@
 package com.spartaschedulerdevelop.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ScheduleSaveRequestDto {
 
-    @NotBlank
+    @NotBlank(message = "이름은 필수입니다.")
+    @Size(min = 2, message = "이름은 2글자 이상이어야 합니다.")
     private String author;
 
-    @NotBlank
+    @NotBlank(message = "제목은 필수입니다.")
+    @Size(max = 30, message = "제목은 30글자 이내여야 합니다.")
     private String title;
 
     @NotBlank
+    @Size(max= 300, message = "내용은 300글자 애녀여야 합니다.")
     private String content;
 
 }
