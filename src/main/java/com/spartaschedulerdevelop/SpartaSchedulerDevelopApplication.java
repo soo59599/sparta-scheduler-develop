7a141c5f494240c940507f6966773af5bc29915d
soo59599 <sooh59599@gmail.com>
2025-08-07 10:07:58 +0900

    chore: 디렉토리 변경

--- src/main/java/com/spartaschedulerdevelop/SpartaSchedulerDevelopApplication.java
+++ src/main/java/com/spartaschedulerdevelop/SpartaSchedulerDevelopApplication.java
@@ -1,4 +1,4 @@
-package com.spartaschedulerdevelop.spartaschedulerdevelop;
+package com.spartaschedulerdevelop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
