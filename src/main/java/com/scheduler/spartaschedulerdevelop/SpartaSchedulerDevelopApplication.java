218ac8981f1f89bb1366118b52be877fd7a9ad47
soo59599 <sooh59599@gmail.com>
2025-08-07 09:43:19 +0900

    chore: 과제 초기 세팅

---
+++ src/main/java/com/scheduler/spartaschedulerdevelop/SpartaSchedulerDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.scheduler.spartaschedulerdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaSchedulerDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaSchedulerDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scheduler/spartaschedulerdevelop/SpartaSchedulerDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduler.spartaschedulerdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaSchedulerDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
