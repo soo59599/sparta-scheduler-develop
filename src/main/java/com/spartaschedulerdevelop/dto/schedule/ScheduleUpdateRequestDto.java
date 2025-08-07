24daa607fe2e9f84ff630ebb9e9a35ccff98050a
soo59599 <sooh59599@gmail.com>
2025-08-07 18:53:22 +0900

    feat: update용 엔티티 작성

---
+++ src/main/java/com/spartaschedulerdevelop/dto/schedule/ScheduleUpdateRequestDto.java
@@ -0,0 +1,12 @@
+package com.spartaschedulerdevelop.dto.schedule;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleUpdateRequestDto {
+
+    private String title;
+
+    private String content;
+
+}
