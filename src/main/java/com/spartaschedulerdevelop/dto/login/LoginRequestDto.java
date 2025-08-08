43d7cf737f53ecc1c346e848d86c1c1acbd6c7e8
soo59599 <sooh59599@gmail.com>
2025-08-09 00:20:06 +0900

    refactor: Login DTO 클래스 → Java Record로 리팩토링

--- src/main/java/com/spartaschedulerdevelop/dto/login/LoginRequestDto.java
+++ src/main/java/com/spartaschedulerdevelop/dto/login/LoginRequestDto.java
@@ -2,15 +2,11 @@ package com.spartaschedulerdevelop.dto.login;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
 
-@Getter
-public class LoginRequestDto {
+public record LoginRequestDto(
+        @Email(message = "이메일 형식에 맞게 작성해야 합니다.")
+        String email,
 
-    @Email(message = "이메일 형식에 맞게 작성해야 합니다.")
-    private String email;
-
-    @NotBlank(message = "비밀번호 입력은 필수입니다.")
-    private String password;
-
-}
+        @NotBlank(message = "비밀번호 입력은 필수입니다.")
+        String password
+) {}
