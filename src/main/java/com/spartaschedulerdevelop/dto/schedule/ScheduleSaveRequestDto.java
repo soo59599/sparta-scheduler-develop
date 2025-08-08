34d88d18982437ceb86f9528eeea762bcd796ac5
soo59599 <sooh59599@gmail.com>
2025-08-08 23:09:07 +0900

    refactor: Schedule DTO 클래스 → Java Record로 리팩토링

--- src/main/java/com/spartaschedulerdevelop/dto/schedule/ScheduleSaveRequestDto.java
+++ src/main/java/com/spartaschedulerdevelop/dto/schedule/ScheduleSaveRequestDto.java
@@ -2,21 +2,15 @@ package com.spartaschedulerdevelop.dto.schedule;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
 
-@Getter
-public class ScheduleSaveRequestDto {
+public record ScheduleSaveRequestDto(
 
-    @NotBlank(message = "이름은 필수입니다.")
-    @Size(min = 2, message = "이름은 2글자 이상이어야 합니다.")
-    private String name;
+        @NotBlank(message = "제목은 필수입니다.")
+        @Size(max = 30, message = "제목은 30글자 이내여야 합니다.")
+        String title,
 
-    @NotBlank(message = "제목은 필수입니다.")
-    @Size(max = 30, message = "제목은 30글자 이내여야 합니다.")
-    private String title;
+        @NotBlank
+        @Size(max = 300, message = "내용은 300글자 이내여야 합니다.")
+        String content
 
-    @NotBlank
-    @Size(max= 300, message = "내용은 300글자 이내여야 합니다.")
-    private String content;
-
-}
+) {}
--- src/main/java/com/spartaschedulerdevelop/dto/schedule/ScheduleUpdateResponseDto.java
+++ src/main/java/com/spartaschedulerdevelop/dto/schedule/ScheduleUpdateResponseDto.java
@@ -1,37 +1,11 @@
 package com.spartaschedulerdevelop.dto.schedule;
 
-import com.spartaschedulerdevelop.entity.Schedule;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-public class ScheduleUpdateResponseDto {
-
-    private final Long id;
-    private final String title;
-    private final String content;
-    private final String name;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime modifiedAt;
-
-    private ScheduleUpdateResponseDto(Long id, String title, String content, String name, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.name = name;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-    }
-
-    public static ScheduleUpdateResponseDto from(Schedule schedule) {
-        return new ScheduleUpdateResponseDto(
-                schedule.getId(),
-                schedule.getTitle(),
-                schedule.getContent(),
-                schedule.getUser().getName(),
-                schedule.getCreatedAt(),
-                schedule.getModifiedAt()
-        );
-    }
-}
+public record ScheduleUpdateResponseDto(
+        Long scheduleId,
+        String title,
+        String content,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt)
+{}
