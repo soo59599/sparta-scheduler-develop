e128b27380b3bb3ff21af48a30ec204827f6c9e4
soo59599 <sooh59599@gmail.com>
2025-08-09 00:11:21 +0900

    refactor: User DTO 클래스 → Java Record로 리팩토링

--- src/main/java/com/spartaschedulerdevelop/dto/user/UserSaveRequestDto.java
+++ src/main/java/com/spartaschedulerdevelop/dto/user/UserSaveRequestDto.java
@@ -3,18 +3,17 @@ package com.spartaschedulerdevelop.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
 
-@Getter
-public class UserSaveRequestDto {
+public record UserSaveRequestDto(
 
-    @NotBlank(message = "이름은 필수입니다.")
-    @Size(min = 2, message = "이름은 2글자 이상이어야 합니다.")
-    private String name;
+        @NotBlank(message = "이름은 필수입니다.")
+        @Size(min = 2, message = "이름은 2글자 이상이어야 합니다.")
+        String name,
 
-    @Email(message = "이메일 형식에 맞게 작성해야 합니다.")
-    private String email;
+        @Email(message = "이메일 형식에 맞게 작성해야 합니다.")
+        String email,
 
-    @NotBlank(message = "비밀번호 입력은 필수입니다.")
-    private String password;
-}
+        @NotBlank(message = "비밀번호 입력은 필수입니다.")
+        String password
+
+) {}
