b884a960f9b056ca012d983e7ad0bfdb294c1b77
soo59599 <sooh59599@gmail.com>
2025-08-08 11:05:21 +0900

    feat: @controller -> @RestController로 변경

--- src/main/java/com/spartaschedulerdevelop/controller/ScheduleController.java
+++ src/main/java/com/spartaschedulerdevelop/controller/ScheduleController.java
@@ -6,12 +6,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/schedules")
 @RequiredArgsConstructor
 public class ScheduleController {
