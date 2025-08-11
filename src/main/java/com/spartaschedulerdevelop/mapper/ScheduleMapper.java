0ff3858526bb51b749563ff5e2ccab490094a6fe
soo59599 <sooh59599@gmail.com>
2025-08-11 16:02:46 +0900

    feat: mapper에 mapping 추가

--- src/main/java/com/spartaschedulerdevelop/mapper/ScheduleMapper.java
+++ src/main/java/com/spartaschedulerdevelop/mapper/ScheduleMapper.java
@@ -5,14 +5,18 @@ import com.spartaschedulerdevelop.dto.schedule.ScheduleSaveResponseDto;
 import com.spartaschedulerdevelop.dto.schedule.ScheduleUpdateResponseDto;
 import com.spartaschedulerdevelop.entity.Schedule;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ScheduleMapper {
 
+    @Mapping(source = "id", target = "scheduleId")
     ScheduleGetOneResponseDto toScheduleGetOneResponseDto (Schedule schedule);
 
+    @Mapping(source = "id", target = "scheduleId")
     ScheduleSaveResponseDto toScheduleSaveResponseDto (Schedule schedule);
 
+    @Mapping(source = "id", target = "scheduleId")
     ScheduleUpdateResponseDto toScheduleUpdateResponseDto (Schedule schedule);
 
 }
--- src/main/java/com/spartaschedulerdevelop/mapper/UserMapper.java
+++ src/main/java/com/spartaschedulerdevelop/mapper/UserMapper.java
@@ -5,13 +5,17 @@ import com.spartaschedulerdevelop.dto.user.UserSaveResponseDto;
 import com.spartaschedulerdevelop.dto.user.UserUpdateResponseDto;
 import com.spartaschedulerdevelop.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(source = "id", target = "userId")
     UserGetOneResponseDto toUserGetOneResponseDto(User user);
 
+    @Mapping(source = "id", target = "userId")
     UserSaveResponseDto toUserSaveResponseDto(User user);
 
+    @Mapping(source = "id", target = "userId")
     UserUpdateResponseDto toUserUpdateResponseDto(User user);
 }
